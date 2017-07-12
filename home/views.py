b8e033119b9759b3066c89dab4c924430305acfb
chamow <chamow97@hotmail.com>
2017-07-13 05:27:15 +0530

    Add events

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, render_to_response
+from . models import events
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
 def about(request):
-    return render(request, 'about.html')
+
+    return render_to_response('about.html', {'events' : events.objects.all()})

ed46d51d680b4c98c44419a56cf893a8ef18e551
chamow <chamow97@hotmail.com>
2017-07-14 12:23:37 +0530

    Change in DB

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,7 @@ app_name = 'home'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about$', views.about, name='about')
+    url(r'^about$', views.about, name='about'),
+    url(r'^past_events$', views.past_events, name='past_events'),
+    url(r'^upcoming_events$', views.upcoming_events, name='upcoming_events')
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,21 @@
+from datetime import date, datetime
+
 from django.shortcuts import render, render_to_response
 from . models import events
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    event = events.objects.filter(start_date__lte=datetime.now()).order_by('start_date')[:5]
+    return render_to_response('index.html', {'events': event})
 
 def about(request):
 
     return render_to_response('about.html', {'events' : events.objects.all()})
+
+def past_events(request):
+    event = events.objects.filter(start_date__lt = datetime.now()).order_by('-start_date')
+    return render_to_response('past_events.html', {'events' : event})
+
+def upcoming_events(request):
+    event = events.objects.filter(start_date__gte = datetime.now()).order_by('start_date')
+    return render_to_response('upcoming_events.html', {'events' : event})

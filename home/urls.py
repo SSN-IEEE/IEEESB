5d7565230be0520a07c6a6eeee3d9f42127c4f73
chamow <chamow97@hotmail.com>
2017-08-15 17:06:02 +0530

    Change table structure

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,7 @@ app_name = 'home'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about$', views.about, name='about'),
-    url(r'^past_events$', views.past_events, name='past_events'),
+    url(r'^past_events$', views.past_events, name='past_events?perPage=100'),
     url(r'^upcoming_events$', views.upcoming_events, name='upcoming_events'),
     url(r'^contact$', views.contact, name='contact'),
     url(r'^gallery$', views.gallery_images, name='gallery_images'),
